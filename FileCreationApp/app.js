c019cf5c2ec113310f7933c083be741321b10e84
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-04-17 16:56:48 +0530

    implement file creation, reading, and deletion functionality in CLI app

---
+++ FileCreationApp/app.js
@@ -0,0 +1,60 @@
+import fs from "fs";
+import readline from "readline";
+
+console.log("1. Create File");
+console.log("2. Read File");
+console.log("3. Delete File");
+console.log("4. Exit");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Choose an option: ", (input) => {
+  fileHandler(input);
+});
+
+const fileHandler = (input) => {
+  if (input === "1") {
+    rl.question("Enter File Name: ", (fileName) => {
+      rl.question("Enter File Content: ", (fileContent) => {
+        fs.writeFile(`${fileName}.txt`, fileContent, (err) => {
+          if (err) {
+            console.log("Error:", err);
+          } else {
+            console.log(`${fileName}.txt created successfully.`);
+          }
+          rl.close();
+        });
+      });
+    });
+  } else if (input === "2") {
+    rl.question("Enter File Name: ", (fileName) => {
+      fs.readFile(`${fileName}.txt`, "utf-8", (err, data) => {
+        if (err) {
+          console.log("Error:", err);
+        } else {
+          console.log("File Content:", data);
+        }
+        rl.close();
+      });
+    });
+  } else if (input === "3") {
+    rl.question("Enter File Name: ", (fileName) => {
+      fs.unlink(`${fileName}.txt`, (err) => {
+        if (err) {
+          console.log("Error:", err);
+        } else {
+          console.log(`${fileName}.txt deleted successfully.`);
+        }
+        rl.close();
+      });
+    });
+  } else if (input === "4") {
+    rl.close();
+  } else {
+    console.log("Invalid Option");
+    rl.close();
+  }
+};
